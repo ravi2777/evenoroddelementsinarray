81370d8c579a51e9e0e5e4261f9af816ed8f6479
hp <hp@RAVIBAGHEL>
2023-08-29 22:15:15 +0530

    Add project files.

---
+++ evenoroddelementsinarray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 2, 3, 4, 5, 6 };
+        int evenCount = 0, oddCount = 0;
+
+        foreach (int num in array)
+        {
+            if (num % 2 == 0)
+            {
+                evenCount++;
+            }
+            else
+            {
+                oddCount++;
+            }
+        }
+
+        Console.WriteLine("Even elements: " + evenCount);
+        Console.WriteLine("Odd elements: " + oddCount);
+    }
+}
